90818b991df41d7654efd6aab0283c1a594543c7
eason_huang <eason_huang@kingdee.com>
2018-09-19 17:21:11 +0800

    feat: 添加注释

--- promise.js
+++ promise.js
@@ -1,8 +1,8 @@
-(function (window) {
+
     var Promise = function (fn) {
         var value = null;
         var callbacks = [];
-        var state = 'pending';
+        var state = 'pending'; // pengding, fulfilled, rejected
 
         var promise = this;
 
@@ -24,35 +24,32 @@
         }
         
         function handle (callback) {
-            // 状态变化前，事件推进队列里
+            // 状态变化前，事件推进队列里；状态一旦变化后不再变动，直接执行结果
             if (state === 'pending') {
                 callbacks.push(callback);
-                return;
-            }
-
-            var cb = state === 'fulfilled' ? callback.onFulfilled : callback.onRejected
-
-            // 没有传递任何值的情况
-            if (!cb) {
-                cb = state === 'fulfilled' ? callback.resolve : callback.reject;
-                cb(value)
-                return;
-            }
-
-            try {
-                // 状态变化后，直接执行事件
-                var ret = cb(value);
-                callback.resolve(ret);
-            } catch (e) {
-                callback.reject(e)
+            } else {
+                var cb = state === 'fulfilled' ? callback.onFulfilled : callback.onRejected
+                // then方法没有传递任何参数的情况下，返回结果值
+                if (!cb) {
+                    cb = state === 'fulfilled' ? callback.resolve : callback.reject;
+                    cb(value)
+                } else {
+                    try {
+                        var ret = cb(value);
+                        callback.resolve(ret);
+                    } catch (e) {
+                        callback.reject(e)
+                    }  
+                }
             }
         }
 
         function resolve(newValue) {
-            // 假如resolve了一个promise的话
+            // 假如resolve了一个promise的话（链式promise）
             if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
                 var then = newValue.then;
                 if (typeof then === 'function') {
+                    // 调用第二个promise中的then，递归直到不是一个promise值为止
                     then.call(newValue, resolve, reject);
                     return
                 }
@@ -104,6 +101,7 @@
     
             function resloveAll (index, value) {
                 result[index] = value
+                // 等待全部promise执行完才执行resolve一个数组
                 if (--count === 0) {
                     resolve(result)
                 }
@@ -133,47 +131,42 @@
             })
         })
     }
-
-    window.Promise = Promise
-
-})(window)
-
  
 /******************************************实例 */
 function test(i) {
     return new Promise(function (resolve) {
         setTimeout(() => {
         resolve(i); 
-        }, 10);
+        }, 1000);
     })
 }
 
 function test2(i) {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
-            if (i > 5) {
+            if (i % 2) {
             resolve(i);  
             } else {
             reject(i); 
             }
-        }, 1000);
+        }, 2000);
     })
 }
 
-test(10).then(test2).then(function (something) {
-    console.log('success' + something);
+test(1).then(test2).then(function (something) {
+    console.log('case1: success!' + something);
 }).catch(function (something) {
-    console.log('failed' + something);
+    console.log('case1: failed!' + something);
 })
 
-Promise.race([test(2), test2(6)]).then(function (something) {
-    console.log('success' + something);
+Promise.all([test(2), test2(4)]).then(function (something) {
+    console.log('case2: success!' + something);
 }).catch(function (something) {
-    console.log('failed' + something)
+    console.log('case2: failed!' + something)
 })
 
-Promise.race([test(3), test2(7)]).then(function (something) {
-    console.log('success' + something);
+Promise.race([test(3), test2(5)]).then(function (something) {
+    console.log('case3: success!' + something);
 }).catch(function (something) {
-    console.log('failed' + something)
+    console.log('case3: failed!' + something)
 })
